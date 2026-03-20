8aa2aeb06f94434751168b3cbcc5735dbad940cc
uparus <36136996+uparus@users.noreply.github.com>
2026-03-20 10:39:12 +0300

    Update Fifteen.cpp

--- Fifteen.cpp
+++ Fifteen.cpp
@@ -1,63 +1,91 @@
 #include <iostream>
 #include <vector>
-#include <conio.h>
-#include "Fifteen.h"
+#include <algorithm>
+#include <random>
+#include <ctime>
+#include "Field.h"
 using namespace std;
 
-Fifteen::Fifteen(size_t size) {
-    game_field.generateStartField(size);
+Field::Field(size_t const& size) : size(size), field(new int[size * size]), zeroIndex(size* size - 1){
+    generateStartField(size);
 }
-int Fifteen::getCount() const { return count; }
-void Fifteen::draw() const {
-    vector<int> field = game_field.getField();
-    int size = game_field.getSize();
 
+bool operator==(Field const& a, Field const& other) {
+    return (a.getSize() == other.getSize() && a.getField() == other.getField() && a.getZeroIndex() == other.getZeroIndex());
+}
+bool operator!=(Field const& a, Field const& other) {
+    return !(a.getSize() == other.getSize() && a.getField() == other.getField() && a.getZeroIndex() == other.getZeroIndex());
+}
+void Field::generateStartField(size_t const& size) {
+    /*this->size = size;
+    field = new int[size * size];*/
+    for (size_t i = 0; i < size * size - 1; i++) {
+        field[i] = i + 1;
+    }
+
+    field[size * size - 1] = 0;
+}
+
+size_t Field::getSize() const { return size; }
+size_t Field::getZeroIndex() const { return zeroIndex; }
+vector<int> Field::getField() const {
+    vector<int> result;
     for (int i = 0; i < size * size; i++) {
-        cout << field[i];
-        if ((i + 1) % size == 0) {
-            cout << endl;
+        result.push_back(field[i]);
+    }
+    return result;
+}
+void Field::shiftUp() {
+    if (zeroIndex >= size) {
+        int newIndex = zeroIndex - size;
+        swap(field[zeroIndex], field[newIndex]);
+        zeroIndex = newIndex;
+    }
+}
+
+void Field::shiftLeft() {
+    if (zeroIndex % size != 0) {
+        int newIndex = zeroIndex - 1;
+        swap(field[zeroIndex], field[newIndex]);
+        zeroIndex = newIndex;
+    }
+}
+void Field::shiftDown() {
+    if (zeroIndex < size * size - size) {
+        int newIndex = zeroIndex + size;
+        swap(field[zeroIndex], field[newIndex]);
+        zeroIndex = newIndex;
+    }
+}
+void Field::shiftRight() {
+    if (zeroIndex % size != size - 1) {
+        int newIndex = zeroIndex + 1;
+        swap(field[zeroIndex], field[newIndex]);
+        zeroIndex = newIndex;
+    }
+}
+
+enum shuffleOption { SHIFT_RIGHT = 1, SHIFT_LEFT = 2, SHIFT_UP = 3, SHIFT_DOWN = 4 };
+void Field::shuffleField() {
+    srand(time(NULL));
+    const int shuffleAmount = 100;
+    for (int i = 0; i < shuffleAmount; i++) {
+        int shuffleOption = 1 + rand() % (4 - 1 + 1);
+        switch (shuffleOption) {
+        case shuffleOption::SHIFT_RIGHT:
+            shiftRight();
+            break;
+        case shuffleOption::SHIFT_LEFT:
+            shiftLeft();
+            break;
+        case shuffleOption::SHIFT_UP:
+            shiftUp();
+            break;
+        case shuffleOption::SHIFT_DOWN:
+            shiftDown();
+            break;
         }
-        else {
-            cout << " ";
-        }
     }
 }
-bool Fifteen::isGameOver() const {
-    vector<int> field = game_field.getField();
-    int size = game_field.getSize();
-
-    for (int i = 0; i < size * size - 1; i++) {
-        if (field[i] != (i + 1)) return false;
-    }
-
-    return field[size * size - 1] == 0;
-}
-void Fifteen::onKeyPressed(int const& btnCone) {
-    switch (btnCone) {
-    case 72: {
-        game_field.shiftUp();
-        count++;
-        break;
-    }
-    case 80: {
-        game_field.shiftDown();
-        count++;
-        break;
-    }
-    case 75: {
-        game_field.shiftLeft();
-        count++;
-        break;
-    }
-    case 77: {
-        game_field.shiftRight();
-        count++;
-        break;
-    }
-    }
-}
-int Fifteen::gameSize() const {
-    int size = game_field.getSize();
-    return size;
-}
-
+     
+   
